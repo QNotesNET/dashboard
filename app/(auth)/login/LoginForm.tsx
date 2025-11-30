7e4adc95b9e5ccd50fbed222d01dde79cfdbe321
Daniel Rockenschaub <daniel.rockenschaub@outlook.com>
2025-11-30 22:28:08 +0100

    testing logo upscale

--- app/(auth)/login/LoginForm.tsx
+++ app/(auth)/login/LoginForm.tsx
@@ -55,7 +55,7 @@ export default function LoginForm()
         <div className="w-full max-w-sm">
           <Image
             alt="Powerbook"
-            src="/images/logos/logo-new-black.svg"
+            src="/images/logos/test.svg"
             width={160}
             height={80}
             priority
