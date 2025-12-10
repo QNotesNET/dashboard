54bfd28ec238acc6c2caf67bec9ad4235d77ea1e
Daniel Rockenschaub <daniel.rockenschaub@outlook.com>
2025-12-10 23:36:58 +0100

    loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,17 @@
 import Loader from "@/components/Loader";
+
 export default function Loading() {
-  return <Loader label="Powrbook lädt…" />;
+  return (
+    <div
+      className="
+        fixed inset-0 
+        flex items-center justify-center 
+        backdrop-blur-sm 
+        bg-white/40
+        z-[9999]
+      "
+    >
+      <Loader />
+    </div>
+  );
 }
